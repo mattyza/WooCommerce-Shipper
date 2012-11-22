2942431c09357aa887360e4169beffc2515fcd4d
Andy Zhang <andy_zhang@hypnoticzoo.com>
2012-11-21 20:20:48 -0500

    Bootstrap class.

---
+++ hypnoticzoo-shipping.php
@@ -0,0 +1,63 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if ( class_exists('HipperShipper') ) { // Exit if framework alreay in use.
+
+class HipperShipper{
+
+    /**
+     * @var string
+     */
+    var $version = '1.0';
+
+
+    /**
+     * HipperShipper Constructor.
+     *
+     * @access public
+     * @return void
+     */
+    function __construct() {
+
+        // Define version constant
+        define( 'HIPPERSHIPPER_VERSION', $this->version );
+
+        // Include required files
+        $this->includes();
+        $this->globals();
+        $this->additional_product_meta();
+
+    }
+
+
+    /**
+     * Include required core files.
+     *
+     * @access public
+     * @return void
+     */
+    function includes() {
+        include( 'class-shipper.php' );     //contains shipping class skeleton
+        include( 'class-xmlparser.php' );   //contains xmlparser class
+        include( 'product-meta.php' );      //contains extra product meta processors
+        include( 'utilities.php' );         //contains utility functions
+    }
+
+    function globals() {
+        $GLOBALS['hipperxmlparser'] = new HipperXMLParser();
+    }
+
+    function additional_product_meta(){
+
+        add_action( 'woocommerce_product_options_dimensions', 'woocommerce_usps_product_girth', 10 );
+        add_action( 'woocommerce_product_options_dimensions', 'woocommerce_usps_product_lettermail', 10 );
+
+        add_action( 'woocommerce_process_product_meta', 'woocommerce_usps_process_product_girth_metabox', 1 );
+        add_action( 'woocommerce_process_product_meta', 'woocommerce_usps_process_product_lettermail_metabox', 1 );
+
+    }
+}
+
+}
+?>
